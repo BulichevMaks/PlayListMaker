e507be89e8f801f971bb044a529f075e94e0e65a
BulichevMaks <formgrav@mail.ru>
2023-02-01 22:18:47 +0100

    third commit to dev

--- app/src/main/java/com/prakticum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/prakticum/playlistmaker/SearchActivity.kt
@@ -1,5 +1,7 @@
 package com.prakticum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,9 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val url = Uri.parse("https://ya.ru")
+        val intent = Intent(Intent.ACTION_VIEW, url)
+        startActivity(intent)
     }
 }
